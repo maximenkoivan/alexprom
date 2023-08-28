afc6e23278b3f1062c114828cc484bb256f5eeb5
maximenkoivan <maximenkoivan83@gmail.com>
2023-08-28 18:58:26 +0300

    fix: добавляет условие отображения jivo-chat

--- local/templates/sneg/header.php
+++ local/templates/sneg/header.php
@@ -42,24 +42,36 @@ $header = CommonData::getInstance()->getElementByCode('basic_settings', true);
         <?php $APPLICATION->ShowPanel(); ?>
     <?php endif; ?>
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(94759803, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true,
-            ecommerce:"dataLayer"
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true,
+            ecommerce: "dataLayer"
         });
     </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/94759803" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/94759803" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
     <!-- /Yandex.Metrika counter -->
-    <script src="//code.jivo.ru/widget/FmEs0fRdxd" async></script>
+    <?php if ($APPLICATION->GetCurPage() == '/'): ?>
+        <script src="//code.jivo.ru/widget/FmEs0fRdxd" async></script>
+    <?php endif; ?>
 </head>
 <body>
 
@@ -76,7 +88,7 @@ $header = CommonData::getInstance()->getElementByCode('basic_settings', true);
             <?php endif; ?>
 
             <div class="header__text">
-                <?= $header['DESC_HEADER']['~VALUE']['TEXT'] ?? ''?>
+                <?= $header['DESC_HEADER']['~VALUE']['TEXT'] ?? '' ?>
             </div>
 
             <div class="spacer"></div>
