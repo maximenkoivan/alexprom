934eeb7a45e1bd0489e361a5b1a0425cd4d13db5
maximenkoivan <maximenkoivan83@gmail.com>
2023-09-14 16:16:25 +0300

    feat: добавляет миграции

---
+++ local/php_interface/migrations/Version20230914161517.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20230914161517 extends Version
+{
+    protected $description   = "Блоки (элементы)";
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->HlblockElementsImport()
+             ->setExchangeResource('hlblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Hlblock()
+                      ->saveElementByXmlId(
+                          $item['hlblock_id'],
+                          $item['fields']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function down()
+    {
+        $this->getExchangeManager()
+             ->HlblockElementsImport()
+             ->setExchangeResource('hlblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Hlblock()
+                      ->deleteElementByXmlId(
+                          $item['hlblock_id'],
+                          $item['fields']['UF_XML_ID']
+                      );
+             });
+    }
+
+
+}
