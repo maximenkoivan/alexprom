1b09d0ff75b3388c89a35330a2c48caf0681943b
m.geleyshev <m.geleyshev@pixelplus.ru>
2023-12-14 15:47:02 +0300

    Правки с боя

---
+++ local/templates/svetmsk/blocks/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="author" content="Контент">
+    <meta name="keywords" content="keywords">
+    <meta name="description" content="description">
+
+    <title>Новогоднее освещение</title>
+
+    <link rel="icon" type="image/png" href="favicon.ico">
+    <link rel="stylesheet" href="/local/templates/svet/assets/vendor.css">
+    <link rel="stylesheet" href="/local/templates/svet/assets/app.min.css">
+
+    <meta property="og:title" content="title"/>
+    <meta property="og:description" content="description"/>
+    <meta property="og:image" content="favicon.png">
+
+</head>
+<body>
+
+<header class="header" data-header>
+    <div class="header__wrap">
+        <div class="container header__container">
+
+            <div class="header__brand">
+                <a href="/" class="brand">
+                    <img alt=" " src="/local/templates/svet/assets/images/logo.png"">
+                </a>
+            </div>
+
+            <div class="header__text">
+                Проектирование и монтаж<br>
+                новогоднего освещения<br>
+                под ключ с <span>гарантией 2 года</span><br>
+            </div>
+
+            <div class="spacer"></div>
+
+            <div class="header__text header__text--small">Санкт-Петербург и Ленобласть</div>
+
+            <div class="header__contacts contacts">
+                <a href="tel:+78122090765" class="contacts__link contacts__link--phone">+7 812 209 07 65</a>
+                <a href="mailto:zakaz@akexprom.ru" class="contacts__link">zakaz@alexprom.ru</a>
+            </div>
+
+            <div class="header__btn">
+                <a data-custom-open="modal-callback" class="btn btn--circle">Связаться</a>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<div class="wrapper">
+    <div class="wrapper__content">
