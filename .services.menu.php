9f4b18281b0efdcea3433cc92aa9d6925c5c8bbc
Sergey <m.geleyshev@pixelplus.ru>
2024-05-23 17:47:25 +0300

    dm_task_61 - прописал ссылки в футоре

--- .services.menu.php
+++ .services.menu.php
@@ -2,21 +2,21 @@
 $aMenuLinks = Array(
 	Array(
 		"Цены", 
-		"/#!/", 
+		"/prices/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Проекты", 
-		"/#!/", 
+		"/projects/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"О компании", 
-		"/#!/", 
+		"/about/", 
 		Array(), 
 		Array(), 
 		"" 
