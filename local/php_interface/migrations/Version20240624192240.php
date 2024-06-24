61f067d6b8cedcb6626b6f120daaf0c91c7f47cd
maximenko <maximenkoivan83@gmail.com>
2024-06-24 19:23:30 +0300

    добавляет миграцию highload блоков

---
+++ local/php_interface/migrations/Version20240624192240.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20240624192240 extends Version
+{
+    protected $description   = "Roofs/Блоки (элементы)";
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->HlblockElementsImport()
+             ->setExchangeResource('hlblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Hlblock()
+                      ->addElement(
+                          $item['hlblock_id'],
+                          $item['fields']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+
+
+}
