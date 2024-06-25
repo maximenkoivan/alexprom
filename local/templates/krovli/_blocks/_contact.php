97d8ffaf15fbee7bf54792caf44e1c923729760d
maximenko <maximenkoivan83@gmail.com>
2024-06-25 21:40:24 +0300

    добавляет блок контакты

--- local/templates/krovli/_blocks/_contact.php
+++ local/templates/krovli/_blocks/_contact.php
@@ -1,7 +1,4 @@
 <?php
-
-use classes\Helpers\Generic;
-
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
@@ -12,34 +9,51 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 ?>
 <section class="sections-contact">
     <div class="title big ta-c">
-        контакты
+        <?= $arResult['PROPERTIES']['TITLE_CONTACT']['~VALUE'] ?>
     </div>
     <div class="map-wrap">
         <div class="address-tab">
             <div class="logo">
-                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/icons/logo.svg" alt="">
+                <?php if (!empty($arResult['PROPERTIES']['LOGO_CONTACT']['VALUE'])): ?>
+                    <img src="<?= CFile::GetPath($arResult['PROPERTIES']['LOGO_CONTACT']['VALUE']) ?>"
+                         alt="<?= strip_tags($arResult['PROPERTIES']['ADDRESS_CONTACT']['~VALUE']) ?>">
+                <?php endif; ?>
             </div>
-            <div class="info">Москва, <span>ул. Бойцовая, д.22</span>, этаж 2, пом.V, комн.9, офис 4 г</div>
-            <div class="email">
-                <div class="ellipse-icon">
-                    <div class="ellipse-icon__pic">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/icons/email.svg" alt="email-icon">
+            <div class="info"><?= $arResult['PROPERTIES']['ADDRESS_CONTACT']['~VALUE'] ?></div>
+            <?php if (!empty($arResult['PROPERTIES']['EMAIL_CONTACT']['~DESCRIPTION'])): ?>
+                <div class="email">
+                    <div class="ellipse-icon">
+                        <div class="ellipse-icon__pic">
+                            <?php if (!empty($arResult['PROPERTIES']['EMAIL_CONTACT']['VALUE'])): ?>
+                                <img src="<?= CFile::GetPath($arResult['PROPERTIES']['EMAIL_CONTACT']['VALUE']) ?>"
+                                     alt="<?= strip_tags($arResult['PROPERTIES']['EMAIL_CONTACT']['~DESCRIPTION']) ?>">
+                            <?php endif; ?>
+                        </div>
                     </div>
+                    <a href="mailto:<?= $arResult['PROPERTIES']['EMAIL_CONTACT']['~DESCRIPTION'] ?>">
+                        <?= $arResult['PROPERTIES']['EMAIL_CONTACT']['~DESCRIPTION'] ?>
+                    </a>
                 </div>
-                zakaz@alexprom.ru
-            </div>
-            <div class="phone">
-                <div class="ellipse-icon">
-                    <div class="ellipse-icon__pic">
-                        <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/icons/phone.svg" alt="phone-icon">
+            <?php endif; ?>
+            <?php if (!empty($arResult['PROPERTIES']['PHONE_CONTACT']['~DESCRIPTION'])): ?>
+                <div class="phone">
+                    <div class="ellipse-icon">
+                        <div class="ellipse-icon__pic">
+                            <?php if (!empty($arResult['PROPERTIES']['PHONE_CONTACT']['VALUE'])): ?>
+                                <img src="<?= CFile::GetPath($arResult['PROPERTIES']['PHONE_CONTACT']['VALUE']) ?>"
+                                     alt="<?= strip_tags($arResult['PROPERTIES']['PHONE_CONTACT']['~DESCRIPTION']) ?>">
+                            <?php endif; ?>
+                        </div>
                     </div>
+                    <a class=""
+                       href="tel:<?= \classes\Helpers\Generic::getCleanPhoneNumber($arResult['PROPERTIES']['PHONE_CONTACT']['~DESCRIPTION']) ?>"><?= $arResult['PROPERTIES']['PHONE_CONTACT']['~DESCRIPTION'] ?></a>
                 </div>
-                <a class="" href="tel:+7 (495) 109-02-42">+7 (495) 109-02-42</a>
-            </div>
+            <?php endif; ?>
         </div>
 
         <div class="map-wrap__inner" id="contact-map" style="position:relative;overflow:hidden;">
-            <iframe src="https://yandex.by/map-widget/v1/?ll=37.720023%2C55.808519&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1Njc2NDY5NhI70KDQvtGB0YHQuNGPLCDQnNC-0YHQutCy0LAsINCR0L7QudGG0L7QstCw0Y8g0YPQu9C40YbQsCwgMjIiCg3l5RZCFQtDX0I%2C&source=serp_navig&z=14.26"  frameborder="0" allowfullscreen="true"></iframe>
+            <iframe src="https://yandex.by/map-widget/v1/?ll=37.720023%2C55.808519&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1Njc2NDY5NhI70KDQvtGB0YHQuNGPLCDQnNC-0YHQutCy0LAsINCR0L7QudGG0L7QstCw0Y8g0YPQu9C40YbQsCwgMjIiCg3l5RZCFQtDX0I%2C&source=serp_navig&z=14.26"
+                    frameborder="0" allowfullscreen="true"></iframe>
             <script>
                 document.addEventListener('click', function (e) {
                     let map = document.querySelector('#contact-map iframe')
@@ -50,5 +64,5 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
                     }
                 })
             </script>
-    </div>
+        </div>
 </section>
