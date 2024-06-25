72b4b092980b9aefc1d5c6b1c914cde86f335c7a
maximenko <maximenkoivan83@gmail.com>
2024-06-25 21:16:47 +0300

    добавляет блок допуслуги

--- local/templates/krovli/_blocks/_add-services.php
+++ local/templates/krovli/_blocks/_add-services.php
@@ -9,109 +9,34 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$addServices = \classes\Models\Roofs\HomePage\AddServices::getInstance()->getAllElements();
 ?>
-<section class="services-sections">
-    <div class="container services">
-        <div class="title big ta-c">
-            <span>Дополнительные</span> услуги
-        </div>
-        <div class="add-services__list">
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#confirm" type="submit">
-                    заказать
-                </button>
+<?php if (!empty($addServices)): ?>
+    <section class="services-sections">
+        <div class="container services">
+            <div class="title big ta-c">
+                <?= $arResult['PROPERTIES']['TITLE_ADD_SERVICES']['~VALUE'] ?>
             </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
-            </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
-            </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
-            </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
-            </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
-            </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
-            </div>
-            <div class="add-services__list--item">
-                <div class="pic">
-                    <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/add-services-bg.png" alt="">
-                </div>
-                <div class="info">
-                    <div class="info__title">Герметизация кровли</div>
-                    <div class="info__price">от 200 р/м2</div>
-                </div>
-                <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#feedback" type="submit">
-                    заказать
-                </button>
+            <div class="add-services__list">
+                <?php foreach ($addServices as $service): ?>
+                    <?php if (empty($service['IMAGE']['VALUE'])) continue ?>
+                    <div class="add-services__list--item">
+                        <div class="pic">
+                            <img src="<?= CFile::GetPath($service['IMAGE']['VALUE']) ?>" alt="<?= $service['NAME'] ?>">
+                        </div>
+                        <div class="info">
+                            <div class="info__title"><?= $service['NAME'] ?></div>
+                            <div class="info__price"><?= $service['IMAGE']['~DESCRIPTION'] ?></div>
+                        </div>
+                        <?php if (!empty($arResult['PROPERTIES']['TEXT_BTN_ADD_SERVICES']['~VALUE'])): ?>
+                            <button class="btn btn-blue" data-bs-toggle="modal" data-bs-target="#confirm" type="submit">
+                                <?= $arResult['PROPERTIES']['TEXT_BTN_ADD_SERVICES']['~VALUE'] ?>
+                            </button>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
+
