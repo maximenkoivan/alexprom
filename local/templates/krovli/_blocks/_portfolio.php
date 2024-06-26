037c0ee6b543e56b4351f5f644d691dcd99ce3d5
maximenko <maximenkoivan83@gmail.com>
2024-06-26 11:15:27 +0300

    добавляет блок портфолио

--- local/templates/krovli/_blocks/_portfolio.php
+++ local/templates/krovli/_blocks/_portfolio.php
@@ -1,7 +1,4 @@
 <?php
-
-use classes\Helpers\Generic;
-
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
@@ -9,152 +6,66 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
  * @var $arResult
  * @global CMain $APPLICATION
  */
+$portfolio = \classes\Models\Roofs\HomePage\Portfolio::getInstance()->getAllElements();
 ?>
-<section class="portfolio-sections">
-    <div class="container portfolio">
-        <div class="title big">
-            портфолио
-        </div>
-        <div class="swiper-outside-wrap">
-            <div data-list-slider="portfolio" class="portfolio-slider">
-                <div class="swiper-wrapper portfolio-slider__wrapper">
-                    <div class="swiper-slide">
-                        <div class="nx-video">
-                            <div class="nx-video__wrapper">
-                                <div class="nx-video__link"
-                                     style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/images/video-box.png')"
-                                     data-video-link="XD5w6r-tqXE"
-                                     data-video-modal="empty">
-                                    <div class="nx-video__icon"></div>
+<?php if (!empty($portfolio)): ?>
+    <section class="portfolio-sections">
+        <div class="container portfolio">
+            <div class="title big">
+                <?= $arResult['PROPERTIES']['TITLE_PORTFOLIO']['~VALUE'] ?>
+            </div>
+            <div class="swiper-outside-wrap">
+                <div data-list-slider="portfolio" class="portfolio-slider">
+                    <div class="swiper-wrapper portfolio-slider__wrapper">
+                        <?php foreach ($portfolio as $items): ?>
+                            <div class="swiper-slide">
+                                <?php foreach ($items['IMAGES']['VALUE'] as $key => $imageId): ?>
+                                    <?php if ($key > 0) continue ?>
+                                    <div class="nx-video">
+                                        <div class="nx-video__wrapper">
+                                            <div class="nx-video__link"
+                                                 style="background-image: url('<?= CFile::GetPath($imageId) ?>')"
+                                                 data-video-link="<?= \classes\Helpers\Generic::getVideoYoutubeID($items['IMAGES']['DESCRIPTION'][$key]) ?>"
+                                                 data-video-modal="empty">
+                                                <div class="nx-video__icon"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                                <div class="pic-box">
+                                    <?php foreach ($items['IMAGES']['VALUE'] as $key => $imageId): ?>
+                                        <?php if ($key === 0) continue ?>
+                                        <a data-fslightbox="portfolio"
+                                           href="<?= CFile::GetPath($imageId) ?>"
+                                           class="pic-box__item">
+                                            <img src="<?= CFile::GetPath($imageId) ?>" alt=" ">
+                                        </a>
+                                    <?php endforeach; ?>
+                                </div>
+                                <div class="info-box">
+                                    <div class="info-box__title">
+                                        <?= $items['NAME'] ?>
+                                    </div>
+                                    <div class="info-box__desc">
+                                        <?= $items['DESCRIPTION']['~VALUE']['TEXT'] ?? '' ?>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="pic-box">
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                        </div>
-                        <div class="info-box">
-                            <div class="info-box__title">
-                                Москва, ул. Тверская, д. 19
-                            </div>
-                            <div class="info-box__desc">
-                                Выполнили обслуживание и дозаправку системы кондиционирования в магазине одежды "United Colors of Benetton".
-                            </div>
-                        </div>
+                        <?php endforeach; ?>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="nx-video">
-                            <div class="nx-video__wrapper">
-                                <div class="nx-video__link"
-                                     style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/images/video-box.png')"
-                                     data-video-link="yVU64UgVMVw"
-                                     data-video-modal="empty">
-                                    <div class="nx-video__icon"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pic-box">
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                        </div>
-                        <div class="info-box">
-                            <div class="info-box__title">
-                                Москва, ул. Тверская, д. 19
-                            </div>
-                            <div class="info-box__desc">
-                                Выполнили обслуживание и дозаправку системы кондиционирования в магазине одежды "United Colors of Benetton".
-                            </div>
-                        </div>
+                    <div class="swiper-pagination"></div>
+                    <div data-list-slider-prev class="portfolio-slider__btn-prev">
+                        <svg width="80" height="80">
+                            <use xlink:href="#icon-caret-left"></use>
+                        </svg>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="nx-video">
-                            <div class="nx-video__wrapper">
-                                <div class="nx-video__link"
-                                     style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/images/video-box.png')"
-                                     data-video-link="XD5w6r-tqXE"
-                                     data-video-modal="empty">
-                                    <div class="nx-video__icon"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pic-box">
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                        </div>
-                        <div class="info-box">
-                            <div class="info-box__title">
-                                Москва, ул. Тверская, д. 19
-                            </div>
-                            <div class="info-box__desc">
-                                Выполнили обслуживание и дозаправку системы кондиционирования в магазине одежды "United Colors of Benetton".
-                            </div>
-                        </div>
+                    <div data-list-slider-next class="portfolio-slider__btn-next">
+                        <svg width="80" height="80">
+                            <use xlink:href="#icon-caret-right"></use>
+                        </svg>
                     </div>
-                    <div class="swiper-slide">
-                        <div class="nx-video">
-                            <div class="nx-video__wrapper">
-                                <div class="nx-video__link"
-                                     style="background-image: url('<?= SITE_TEMPLATE_PATH ?>/assets/images/video-box.png')"
-                                     data-video-link="XD5w6r-tqXE"
-                                     data-video-modal="empty">
-                                    <div class="nx-video__icon"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pic-box">
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                            <a data-fslightbox="portfolio" href="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" class="pic-box__item">
-                                <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/pic-box.png" alt=" ">
-                            </a>
-                        </div>
-                        <div class="info-box">
-                            <div class="info-box__title">
-                                Москва, ул. Тверская, д. 19
-                            </div>
-                            <div class="info-box__desc">
-                                Выполнили обслуживание и дозаправку системы кондиционирования в магазине одежды "United Colors of Benetton".
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-pagination"></div>
-                <div data-list-slider-prev class="portfolio-slider__btn-prev">
-                    <svg width="80" height="80">
-                        <use xlink:href="#icon-caret-left"></use>
-                    </svg>
-                </div>
-                <div data-list-slider-next class="portfolio-slider__btn-next">
-                    <svg width="80" height="80">
-                        <use xlink:href="#icon-caret-right"></use>
-                    </svg>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
